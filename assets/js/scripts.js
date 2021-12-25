86c12e4a1b530074654a53c375b7dc3a8e56ea26
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2021-12-25 20:38:50 -0300

    Adicionando evento e salvando mensagem do tweet

---
+++ assets/js/scripts.js
@@ -0,0 +1,15 @@
+const textarea = document.querySelector('textarea')
+const tweetar = document.querySelector('button')
+
+function salvarTweet (event) {
+    event.preventDefault()
+    
+    const tweetTextArea = textarea.value
+}
+
+tweetar.addEventListener('click', salvarTweet)
+
+
+
+
+
