8456ef2edd6a479c500f3bf551c8ee390971cb62
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2021-12-25 20:59:38 -0300

    Armazendndo informações do tweet

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,12 +4,31 @@ const tweetar = document.querySelector('button')
 function salvarTweet (event) {
     event.preventDefault()
     
-    const tweetTextArea = textarea.value
+    const tweetTextArea = textarea.value;
+    criarTweet(tweetTextArea)
 }
 
 tweetar.addEventListener('click', salvarTweet)
 
+function criarTweet (tweetTexto) {
 
+    let data = new Date()
+    let hora = data.getHours()
+    let minutos = data.getMinutes()
 
+    const tweet = {
+        nome: 'Daniel',
+        foto: './assets/img/ProfilePic.png',
+        usuario: '@danielkenzie',
+        texto: tweetTexto,
+        tempo: `${hora}:${minutos}`
+    }
+
+    listarTweet(tweet)
+}
+
+function listarTweet (tweet) {
+
+}
 
 
