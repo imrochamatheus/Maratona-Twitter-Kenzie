29296c843af28566f7c58cfc0f554071530da33c
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2021-12-25 22:08:34 -0300

    Finalizando script para postar tweet

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,6 @@
 const textarea = document.querySelector('textarea')
 const tweetar = document.querySelector('button')
+const feed = document.querySelector('.conteudoPrincipal__listaTweets')
 
 function salvarTweet (event) {
     event.preventDefault()
@@ -24,11 +25,43 @@ function criarTweet (tweetTexto) {
         tempo: `${hora}:${minutos}`
     }
 
-    listarTweet(tweet)
+    listarTemplateTweet(tweet)
 }
 
-function listarTweet (tweet) {
+function listarTemplateTweet (tweet) {
+    
+    const {nome, foto, usuario, texto, tempo} = tweet
 
+    const li    = document.createElement('li')
+    const img   = document.createElement('img')
+    const div   = document.createElement('div')
+    const h2    = document.createElement('h2')
+    const span  = document.createElement('span')
+    const p     = document.createElement('p')
+
+    div.appendChild(h2)
+    h2.innerText = nome
+
+    div.appendChild(span)
+    span.innerText = ` ${usuario} - ${tempo}`
+
+    div.appendChild(p)
+    p.innerText = texto
+
+    li.appendChild(img)
+    img.src = foto
+
+    li.appendChild(div)
+
+
+
+    li.classList.add('conteudoPrincipal__tweet')
+    img.classList.add('tweet__fotoPerfil')
+    div.classList.add('tweet__conteudo')
+ 
+
+
+    feed.appendChild(li)
 }
 
 
